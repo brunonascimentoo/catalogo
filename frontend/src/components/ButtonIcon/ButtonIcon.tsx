7e3162b04c9acdfaea5602cb6e0976d753130331
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-15 22:12:55 -0300

    botão customizado

--- frontend/src/components/ButtonIcon/ButtonIcon.tsx
+++ frontend/src/components/ButtonIcon/ButtonIcon.tsx
@@ -1,9 +1,15 @@
 import './styles.css';
+import ArrowImg from '../../assets/images/arrow.svg';
 
 export function ButtonIcon() {
   return (
-    <button className="btn btn-primary btn-icon">
-      <h6> inicie agora a sua busca</h6>
-    </button>
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>inicie agora a sua busca</h6>
+      </button>
+      <div className="btn-icon-container">
+        <img src={ArrowImg} />
+      </div>
+    </div>
   );
 }
