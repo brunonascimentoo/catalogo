371e7c73de595b0d4bdc1dd6736e9f9539cc8e04
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-18 14:57:48 -0300

    ProductDetails routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { Catalogo } from './pages/Catalogo/Catalogo';
 import { Admin } from './pages/Admin/Admin';
+import { ProductDetails } from './pages/ProductDetails/ProductDetails';
 
 export function Routes() {
   return (
@@ -12,8 +13,11 @@ export function Routes() {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
           <Catalogo />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetails />
         </Route>
         <Route path="/admin">
           <Admin />
---
+++ frontend/src/pages/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export function ProductDetails() {
+  return <h1>Ola pagina</h1>;
+}
