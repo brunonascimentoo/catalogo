61b01ef57482af9ad21b3caab067d51a0aec3ca0
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-19 22:09:47 -0300

    useState, useEffect, useParams

--- frontend/src/pages/ProductDetails/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails/ProductDetails.tsx
@@ -1,9 +1,27 @@
 import './styles.css';
 import arrowImg from '../../assets/images/arrow.svg';
 import { ProductPrice } from '../../components/ProductPrice/ProductPrice';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/product';
+import axios from 'axios';
+import { BASE_URL } from '../../util/requests';
+
+interface UlrParams {
+  productId: string;
+}
 
 export function ProductDetails() {
+  const { productId } = useParams<UlrParams>();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`).then((response) => {
+      setProduct(response.data);
+    });
+  }, [product]);
+
   return (
     <div className="product-details-container">
       <div className="product-details-card base-card">
@@ -16,23 +34,17 @@ export function ProductDetails() {
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-              <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
-                alt="nome do produto"
-              />
+              <img src={product?.imgUrl} alt={product?.name} />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do Produto</h2>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati, id!
-              </p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
